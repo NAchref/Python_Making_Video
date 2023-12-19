fd49468e2cc65fcb6d21acf2e164cc6ea36bb951
NAchref <achref.nefzaoui@esprit.tn>
2023-12-19 21:47:13 +0100

    Devops interview questions

---
+++ app.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+import random
+import os
+import openai
+from gtts import gTTS
+from moviepy.editor import *
+import moviepy.video.fx.crop as crop_vid
+load_dotenv()
+
+# Ask for video information (title,content)
+title = input("\nEnter the name of the video >  ")
+option = input('Do you want AI to generate content? (yes/no) >  ')
+
+if option == 'yes':
+    # Generate content using OpenAI API
+    theme = input("\nEnter the theme of the video >  ")
+
+    ### MAKE .env FILE AND SAVE YOUR API KEY ###
+    openai.api_key = os.environ["OPENAI_API"]
+    response = openai.Completion.create(
+        engine="gpt-3.5-turbo-instruct",
+        prompt=f"Generate content on - \"{theme}\"",
+        temperature=0.7,
+        max_tokens=200,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    print(response.choices[0].text)
+
+    yes_no = input('\nIs this fine? (yes/no) >  ')
+    if yes_no == 'yes':
+        content = response.choices[0].text
+    else:
+        content = input('\nEnter >  ')
+else:
+    content = input('\nEnter the content of the video >  ')
+
+
